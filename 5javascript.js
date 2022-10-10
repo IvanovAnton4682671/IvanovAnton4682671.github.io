162461618544cce1bc87486af361aa115dd62259
Ivanov Anton <112862863+IvanovAnton4682671@users.noreply.github.com>
2022-10-10 13:33:03 +0300

    Update 5javascript.js

--- 5javascript.js
+++ 5javascript.js
@@ -1,16 +1,21 @@
 function multiply() {
     var num1, num2, result;
     num1 = document.getElementById('n1').value;
-    num1 = parseInt(num1);
     num2 = document.getElementById('n2').value;
+    var regex = /^[1-9]\d+$/;
+    if (!regex.test(num1) || !regex.test(num2)) {
+        alert("Пожалуйста, введите корректные числа.");
+        return false;
+    }
+    num1 = parseInt(num1);
     num2 = parseInt(num2);
     result = num1 * num2;
-    alert("Вы собираетесь умножить 2 числа: "+num1+" и "+num2);
-    document.getElementById('out').innerHTML = result;
+    alert("Вы собираетесь умножить 2 числа: "+num1+" и "+num2+".");
+    document.getElementById('out').innerHTML = "Результат: "+result;
 }
 
 window.addEventListener('DOMContentLoaded', function (event) {
     console.log("DOM fully loaded and parsed");
     let b = document.getElementById("click");
-    b.addEventListener("multiply");
+    b.addEventListener("Пожалуйста, введите корректные числа.", multiply);
 });
